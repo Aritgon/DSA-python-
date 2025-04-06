0f23309d24293f66fab4837fb642eae9a65fec64
Aritgon <aritgon1947@gmail.com>
2025-04-06 11:37:47 +0530

    added descending order bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -12,4 +12,20 @@ def bubbleSort(arr):
     return arr
 
 array = [5,9,1,6,3]
-print("sorted array: ", bubbleSort(array))
+print("sorted array(ascending order): ", bubbleSort(array))
+
+
+# bubble sort in descending order.
+def desc_bubblesort(arr):
+
+    n = len(arr)
+
+    for i in range(n): # process to run n times.
+        for j in range(0, n - i - 1): # avoid already sorted items.
+            if arr[j] < arr[j+1]:
+                # swap those.
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+array = [5,6,9,1,3]
+print("Sorted array(descending order): ", desc_bubblesort(array))
